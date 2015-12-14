8ebcd7580bf06477be03c766044313c0a6fbb106
Mato Ilic <mat@ti8m.ch>
2015-12-14 14:55:50 +0100

    use space-after-keywords

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
 module.exports = {
-    "plugins": [
-        "eslint-config-angular-lazy/rules"
-    ],
     'extends': [
         'eslint-config-airbnb/base'
     ],
-    "env": {
-        "browser": true,
-        "node": true,
-        "jasmine": true,
-        "protractor": true
+    env: {
+        browser: true,
+        node: true,
+        jasmine: true,
+        protractor: true
     },
-    "rules": {
-        "indent": [2, 4],
-        "comma-dangle": [2, "never"],
-        "func-names": 0,
-        "space-before-keywords": 0,
-        "space-before-keywords-no-paren": 1
+    rules: {
+        indent: [2, 4],
+        'comma-dangle': [2, 'never'],
+        'func-names': 0
     }
 };
