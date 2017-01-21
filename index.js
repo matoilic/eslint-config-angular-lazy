addb1b65a4769201b40580e442f47a44708badfc
Mato Ilic <info@matoilic.ch>
2017-01-21 20:44:15 +0100

    fix some isses introduced in 0.2.0

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
     extends: [
-        'eslint-config-airbnb/base'
+        'eslint-config-airbnb-base'
     ],
     env: {
         browser: true,
@@ -10,8 +10,12 @@ module.exports = {
     },
     rules: {
         indent: [2, 4],
+        'class-methods-use-this': 'off',
         'comma-dangle': [2, 'never'],
         'func-names': 0,
+        'import/first': 'off',
+        'import/no-extraneous-dependencies': 'off',
+        'no-underscore-dangle': 'off',
         'max-len': [2, 120, 2, {
             ignoreUrls: true,
             ignoreComments: true
